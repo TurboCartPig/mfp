4042829b8ade0cccbe494ed2c4e71b3eaafbf351
Dennis Kristiansen <denniskristiansen@protonmail.com>
2020-04-22 00:32:33 +0200

    Fix Problem 11.3 and format the file

--- haskell/src/Problem11.hs
+++ haskell/src/Problem11.hs
@@ -17,17 +17,6 @@ dice = [1 .. 6]
 sample1 :: [[Int]]
 sample1 = [[i, j, k, l] | i <- dice, j <- dice, k <- dice, l <- dice]
 
--- Sample space of 3 cards in a hand
-deck :: [Int]
-deck = [1 .. 52]
-
-sample2 :: [[Int]]
-sample2 = [[i, j, k] | i <- deck, j <- [i + 1 .. 52], k <- [j + 1 .. 52]]
-
--- Sample space of 4 people in a queue
-sample3 :: [String]
-sample3 = permutations ['A', 'B', 'C', 'D']
-
 -- Problem 11.2
 event1 :: [[Int]]
 event1 = [s | s <- sample1, length (filter (== 6) s) >= 2]
@@ -36,13 +25,24 @@ probability1 :: Double
 probability1 = probability event1 sample1
 
 -- Problem 11.3
+-- Sample space of 3 cards in a hand
+sample2 :: [[Int]]
+sample2 = [[i, j, k] | i <- [0 .. 52], j <- [i + 1 .. 51], k <- [j + 1 .. 51]]
+
+cardValue :: Int -> Int
+cardValue card = card `div` 4
+
 event2 :: [[Int]]
-event2 = [s | s <- sample2, length (filter (== 11) s) >= 3]
+event2 = [s | s <- sample2, all (\x -> cardValue x == 11) s]
 
 probability2 :: Double
 probability2 = probability event2 sample2
 
 -- Problem 11.4
+-- Sample space of 4 people in a queue
+sample3 :: [String]
+sample3 = permutations ['A', 'B', 'C', 'D']
+
 -- Why do I not have to deal with the fact that elemIndex can fail to lookup?
 -- Is that simply part of list comprehension?
 -- elemIndex -> Nothing == failed to satisfy predicate?
