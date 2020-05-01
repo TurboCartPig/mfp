0dd968438ae1b59df853f5cdf76e7001a3dba106
Dennis Kristiansen <denniskristiansen@protonmail.com>
2020-05-01 22:45:36 +0200

    Add readline example

--- haskell/src/Main.hs
+++ haskell/src/Main.hs
@@ -2,3 +2,9 @@ module Main where
 
 main :: IO ()
 main = putStrLn "Hello!"
+
+readExample :: IO ()
+readExample = do
+  thing <- getLine
+  putStrLn ("What the fuck is \"" ++ thing ++ "\"?")
+  return ()
