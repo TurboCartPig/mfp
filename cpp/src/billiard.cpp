b552e9e24ec64880eeaf411dba7860b26dcacead
Dennis Kristiansen <denniskristiansen@protonmail.com>
2020-05-07 00:11:37 +0200

    Refractor billiard exercise

--- cpp/src/billiard.cpp
+++ cpp/src/billiard.cpp
@@ -13,39 +13,128 @@
 
 // Constants
 // ***********************************************************************
-static const uint32_t windowx     = 1000;
-static const uint32_t windowy     = 1000;
-static const float    BALL_RADIUS = 10.0f;
-static const size_t   BALL_COUNT  = 20;
+static const uint32_t windowx          = 1000;
+static const uint32_t windowy          = 1000;
+static const float    BALL_RADIUS      = 10.0f;
+static const uint32_t BALL_POINT_COUNT = 10;
+static const size_t   BALL_COUNT       = 20;
 
 // Globals
 // ***********************************************************************
 
-std::vector<sf::CircleShape *> gBalls;
-std::vector<sf::Vector2f>      gVels;
-std::function<float()>         rnd;
+// std::vector<sf::CircleShape *> gBalls;
+// std::vector<sf::Vector2f>      gVels;
+std::vector<class Ball> gBalls;
+std::function<float()>  rnd;
 
 // Declarations
 // ***********************************************************************
 
+/**
+ * Axis Aligned Bounding Box.
+ */
+class AABB {
+  public:
+	AABB(sf::Vector2f min, sf::Vector2f max) {
+		this->min = min;
+		this->max = max;
+	}
+
+	//  private:
+	sf::Vector2f min;
+	sf::Vector2f max;
+};
+
+/**
+ * Billiard ball.
+ */
 class Ball {
   public:
+	/// Construct a new ball with a given position
+	Ball(const sf::Vector2f pos, const sf::Vector2f vel) {
+		this->shape = sf::CircleShape(BALL_RADIUS, BALL_POINT_COUNT);
+		shape.setFillColor(sf::Color::Red);
+		this->pos  = pos;
+		this->vel  = vel;
+		this->mass = 1;
+	}
+
+	/// Update the internal state of the ball
+	void update(const float dt) {
+		// Integration
+		pos = pos + vel * dt;
+
+		shape.setPosition(pos);
+	}
+
+	/// Draw the ball
+	void draw(sf::RenderWindow &w) { w.draw(this->shape); }
+
+	/// Is this ball intersecting another ball?
+	bool isIntersecting(const Ball &other) {
+		auto minD = this->shape.getRadius() + other.shape.getRadius();
+		auto dis  = length(this->pos - other.pos);
+
+		return minD >= dis;
+	}
+
+	/// Is this ball intersecting another AABB?
+	// FIXME: Do I even need this?
+	//	bool isIntersecting(const AABB &other) {
+	//		auto rad = shape.getRadius();
+	//
+	//		return false;
+	//	}
+
+	/// Perform collision between Ball and AABB if intersecting
+	void collide(const AABB &other) {
+		auto rad = shape.getRadius();
+
+		if (pos.x >= other.max.x - rad * 2) {
+			pos.x = other.max.x - rad * 2;
+			vel.x = -vel.x;
+		} else if (pos.x <= other.min.x) {
+			pos.x = other.min.x;
+			vel.x = -vel.x;
+		}
+
+		if (pos.y >= other.max.y - rad * 2) {
+			pos.y = other.max.y - rad * 2;
+			vel.y = -vel.y;
+		} else if (pos.y <= other.min.y) {
+			pos.y = other.min.y;
+			vel.y = -vel.y;
+		}
+
+		shape.setPosition(pos);
+	}
+
+	/// Perform collision between two balls if intersecting
+	void collide(const Ball &other) {
+		if (!isIntersecting(other))
+			return;
+
+		std::cout << "Boink!\n";
+
+		// TODO: Now perform elastic collision
+	}
+
+	void setPosition(const sf::Vector2f pos) {
+		this->pos = pos;
+		shape.setPosition(pos);
+	}
+
+  private:
 	sf::CircleShape shape;
 	sf::Vector2f    pos;
 	sf::Vector2f    vel;
 	float           mass;
 };
 
-// Helper Functions
-// ***********************************************************************
-
-bool isColiding(sf::CircleShape &a, sf::CircleShape &b) {
-	auto minD = a.getRadius() + b.getRadius();
-	auto dis  = length(b.getPosition() - a.getPosition());
-
-	return minD >= dis;
-}
-
+/**
+ * Main entry point.
+ * @return EXIT_SUCCESS
+ */
 int main() {
 	// Init randomness
 	// *******************************************************************
@@ -77,34 +166,37 @@ int main() {
 	// Init simulation stuff
 	// *******************************************************************
 
+	// Create screen bounding box
+	auto screen =
+	    AABB(sf::Vector2f(0.0f, 0.0f), sf::Vector2f(windowx, windowy));
+
+	// Spawn all the balls
 	gBalls.reserve(BALL_COUNT);
-	gVels.reserve(BALL_COUNT);
 	for (size_t i = 0; i < BALL_COUNT; i++) {
-		auto ball = new sf::CircleShape(BALL_RADIUS, 10);
-		ball->setFillColor(sf::Color::Red);
-		ball->setPosition(windowx / 2, windowy / 2);
+		auto pos = sf::Vector2f(windowx / 2, windowy / 2);
 
 		float speed = 100.0f;
-		gVels[i]    = speed * sf::Vector2f(rnd(), rnd());
+		auto  vel   = speed * sf::Vector2f(rnd(), rnd());
 
+		auto ball = Ball(pos, vel);
 		gBalls.push_back(ball);
 	}
 
 	// Distribute the balls around the screen without intersections
 	for (size_t i = 0; i < gBalls.size(); i++) {
-		auto ball      = gBalls[i];
-		bool colliding = false;
+		bool colliding;
 
-		ball->setPosition((rnd() + 1) * windowx / 2, (rnd() + 1) * windowy / 2);
-		for (auto other : gBalls) {
-			if (ball == other)
-				continue;
-			if (isColiding(*ball, *other))
-				colliding = true;
-		}
+		do {
+			colliding = false;
+			gBalls[i].setPosition(sf::Vector2f((rnd() + 1) * windowx / 2,
+			                                   (rnd() + 1) * windowy / 2));
+			for (size_t j = 0; j < gBalls.size(); j++) {
+				if (i == j)
+					continue;
 
-		if (colliding)
-			i--;
+				colliding = gBalls[i].isIntersecting(gBalls[j]);
+			}
+		} while (colliding);
 	}
 
 	float     time = 0.0f;
@@ -137,47 +229,24 @@ int main() {
 		// Simulation
 		// ***************************************************************
 
-		// Integration
-		for (size_t i = 0; i < BALL_COUNT; i++) {
-			gBalls[i]->setPosition(gBalls[i]->getPosition() + gVels[i] * dt);
+		// Collisions
+		for (auto &ball : gBalls) {
+			// Collide with wall
+			ball.collide(screen);
+
+			// Collide with other balls
+			for (const auto &other : gBalls) {
+				// Skip if self
+				if (&ball == &other)
+					continue;
+
+                ball.collide(other);
+			}
 		}
 
-		// Check for intersections
-		for (size_t i = 0; i < BALL_COUNT; i++) {
-			auto ball  = gBalls[i];
-			auto mass1 = 1;
-			auto mass2 = 1;
-
-			// Collision with other balls
-			for (size_t j = 0; j < BALL_COUNT; j++) {
-				if (i == j)
-					continue;
-				if (isColiding(*ball, *gBalls[j])) {
-					std::cout << "Boink!";
-					// Perform collision
-					auto u2 = 
-                    auto v1 = (mass1 - mass2) / (mass1 + mass2) + (2 * mass2) / (mass1 * mass2) * u2;
-				}
-			}
-
-			// Collision with walls
-			auto pos = ball->getPosition();
-			auto rad = ball->getRadius();
-			if (pos.x >= windowx - rad * 2) {
-				pos.x      = windowx - rad * 2;
-				gVels[i].x = -gVels[i].x;
-			} else if (pos.x <= 0.0f) {
-				pos.x      = 0.0f;
-				gVels[i].x = -gVels[i].x;
-			}
-			if (pos.y >= windowy - rad * 2) {
-				pos.y      = windowy - rad * 2;
-				gVels[i].y = -gVels[i].y;
-			} else if (pos.y <= 0.0f) {
-				pos.y      = 0.0f;
-				gVels[i].y = -gVels[i].y;
-			}
-			ball->setPosition(pos);
+		// Normal update
+		for (auto &ball : gBalls) {
+			ball.update(dt);
 		}
 
 		// Rendering
@@ -185,9 +254,8 @@ int main() {
 
 		window.clear();
 
-		for (auto ball : gBalls) {
-			window.draw(*ball);
-		}
+		for (auto &ball : gBalls)
+			ball.draw(window);
 
 		window.display();
 	}
