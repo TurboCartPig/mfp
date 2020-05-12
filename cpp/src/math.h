d7503abc6e7f59b43737d1ba5b942ad60b662075
Dennis Kristiansen <denniskristiansen@protonmail.com>
2020-05-12 18:37:52 +0200

    Add almost_equal()

--- cpp/src/math.h
+++ cpp/src/math.h
@@ -17,7 +17,12 @@
  */
 template <typename T>
 T map_range(T v, T in_min, T in_max, T out_min, T out_max) {
-    return (v - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+	return (v - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+template <typename T>
+T signum(T a) {
+	return a >= 0 ? 1 : -1;
 }
 
 /**
@@ -48,7 +53,7 @@ sf::Vector2f limit(const sf::Vector2f v, float len) {
  * @return  The dot product
  */
 float dot(const sf::Vector2f a, const sf::Vector2f b) {
-    return a.x * b.x + a.y * b.y;
+	return a.x * b.x + a.y * b.y;
 }
 
 /**
@@ -59,7 +64,7 @@ float dot(const sf::Vector2f a, const sf::Vector2f b) {
  * @return  The projection vector
  */
 sf::Vector2f project(const sf::Vector2f a, const sf::Vector2f b) {
-    return b * dot(a, b) / (length(b) * length(b));
+	return b * dot(a, b) / (length(b) * length(b));
 }
 
 /**
@@ -68,6 +73,25 @@ sf::Vector2f project(const sf::Vector2f a, const sf::Vector2f b) {
  * @param vec Vector to print
  * @return    Output stream with vector pretty printed
  */
-std::ostream& operator<<(std::ostream &os, const sf::Vector2f &vec) {
+std::ostream &operator<<(std::ostream &os, const sf::Vector2f &vec) {
 	return os << "[" << vec.x << "," << vec.y << "]";
+}
+
+/**
+ * Are the parameters almost equal?
+ *
+ * @param x
+ * @param y
+ * @param ulp acceptable deviation
+ * @return    are they?
+ */
+template <class T>
+// typename std::enable_if<!std::numeric_limits<T>::is_integer, bool>::type
+bool almost_equal(T x, T y, int ulp) {
+	// the machine epsilon has to be scaled to the magnitude of the values used
+	// and multiplied by the desired precision in ULPs (units in the last place)
+	return std::fabs(x - y) <=
+	           std::numeric_limits<T>::epsilon() * std::fabs(x + y) * ulp
+	       // unless the result is subnormal
+	       || std::fabs(x - y) < std::numeric_limits<T>::min();
 }
